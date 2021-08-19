2268cf2289a6428232eb1a6a7bfd416cda30babc
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2021-08-19 14:40:26 +0530

    Files added

---
+++ hex.js
@@ -0,0 +1,28 @@
+const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+const changeBtn = document.getElementById("changeBtn")
+const colorDisplay = document.getElementById("color-el")
+
+
+clickFunc()
+changeBtn.addEventListener('click',clickFunc)
+
+function clickFunc(){
+    let hexCode = getRandomHex()
+    document.body.style.backgroundColor = hexCode
+    colorDisplay.textContent = hexCode
+    console.log(hexCode)
+}
+
+function getRandomHex(){
+    let temp = '#'
+    for(let i=0; i<6; i++){
+        let t = getRandomNumber(hex.length)
+        temp += hex[t]
+    }
+    return temp     
+}
+
+function getRandomNumber(n){
+    let temp = Math.floor(Math.random() * n)
+    return temp
+}
